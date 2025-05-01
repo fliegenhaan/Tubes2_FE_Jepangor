d2f1d82052e6bb50734564e8868e0e8b1ab7f6b3
Muhammad Raihaan Perdana <perdanaraihan96@gmail.com>
2025-05-01 13:47:05 +0700

    Initial set up for next.js (frontend)

---
+++ jepangor/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-[url('/placeholder.png')] bg-cover bg-center bg-no-repeat min-h-screen">
+      Hello Welcome to Jepangor
+    </div>
+  )
+}
+
+export default page
